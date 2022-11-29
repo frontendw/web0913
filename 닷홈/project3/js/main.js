742b7f48ad1b9d199c5036d7d4cb43dda0370436
frontendw <whyeil@nate.com>
2022-11-29 12:11:05 +0900

    1129

--- 닷홈/project3/js/main.js
+++ 닷홈/project3/js/main.js
@@ -25,4 +25,23 @@ $(function () {
         captions: true
     });
 
+    // 섹션3: 스크롤 모션
+    $(window).scroll(function(){
+        let wst = $(window).scrollTop(); // 창의 스크롤 탑 값
+             $('.num').text('스크롤 량=' + wst);
+
+        if (wst > 600) {
+            $('.motion1').stop().animate({top: 0}, 500, 'easeOutExpo')
+            $('.motion2').stop().animate({top: 0}, 600, 'easeOutExpo')
+            $('.motion3').stop().animate({top: 0}, 800, 'easeOutExpo')
+            $('.motion4').stop().animate({top: 0}, 1000, 'easeOutExpo')
+        } else {
+            $('.motion1').stop().animate({top: 1000})
+            $('.motion2').stop().animate({top: 2000})
+            $('.motion3').stop().animate({top: 3000})
+            $('.motion4').stop().animate({top: 4000})
+        }
+
+    }); // scroll()
+
 }); // ready()
